4e62a3d8e52b4f9c2b5c77596b2f5e0736ecbbb3
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-09-24 08:45:49 +0200

    Formatted code.

--- procalign/src/main/java/fr/inrialpes/exmo/align/impl/ObjectCell.java
+++ procalign/src/main/java/fr/inrialpes/exmo/align/impl/ObjectCell.java
@@ -48,78 +48,91 @@ public class ObjectCell extends BasicCell {
     //  visitor.visit( this );
     //}
 
-    /** Creation **/
-    public ObjectCell( String id, Object ob1, Object ob2, Relation rel, double m ) throws AlignmentException {
-	super( id, ob1, ob2, rel, m );
-    };
+    /**
+     * Creation *
+     */
+    public ObjectCell(String id, Object ob1, Object ob2, Relation rel, double m) throws AlignmentException {
+        super(id, ob1, ob2, rel, m);
+    }
+
+    ;
 
     /**
      * Used to order the cells in an alignment:
      * -- this > c iff this.getStrength() < c.getStrength() --
-    public int compareTo( Cell c ){
-	//if ( ! (c instanceof Cell) ) return 1;
-	if ( c.getStrength() > getStrength() ) return 1;
-	if ( getStrength() > c.getStrength() ) return -1;
-	return 0;
-    }
+     * public int compareTo( Cell c ){
+     * //if ( ! (c instanceof Cell) ) return 1;
+     * if ( c.getStrength() > getStrength() ) return 1;
+     * if ( getStrength() > c.getStrength() ) return -1;
+     * return 0;
+     * }
      */
 
-    public URI getObject1AsURI( Alignment al ) throws AlignmentException {
-	if ( al instanceof BasicAlignment ) {
-	    Object ontology = ((BasicAlignment)al).getOntologyObject1();
-	    if ( ontology instanceof LoadedOntology ) {
-		try {
-		    return ((LoadedOntology)ontology).getEntityURI( object1 );
-		} catch ( OntowrapException owex ) {
-		    throw new AlignmentException( "Cannot find entity URI(1)", owex );
-		}
-	    }
-	};
-	if ( object1 instanceof URI ) {
-	    return (URI)object1;
-	} else {
-	    throw new AlignmentException( "Cannot find URI for "+object1 );
-	}
+    public URI getObject1AsURI(Alignment al) throws AlignmentException {
+        if (al instanceof BasicAlignment) {
+            Object ontology = ((BasicAlignment) al).getOntologyObject1();
+            if (ontology instanceof LoadedOntology) {
+                try {
+                    return ((LoadedOntology) ontology).getEntityURI(object1);
+                } catch (OntowrapException owex) {
+                    throw new AlignmentException("Cannot find entity URI(1)", owex);
+                }
+            }
+        }
+        ;
+        if (object1 instanceof URI) {
+            return (URI) object1;
+        } else {
+            throw new AlignmentException("Cannot find URI for " + object1);
+        }
     }
-    public URI getObject2AsURI( Alignment al ) throws AlignmentException {
-	if ( al instanceof BasicAlignment ) {
-	    Object ontology = ((BasicAlignment)al).getOntologyObject2();
-	    if ( ontology instanceof LoadedOntology ) {
-		try {
-		    return ((LoadedOntology)ontology).getEntityURI( object2 );
-		} catch ( OntowrapException owex ) {
-		    throw new AlignmentException( "Cannot find entity URI(2)", owex );
-		}
-	    }
-	};
-        if ( object2 instanceof URI ) {
-	    return (URI)object2;
-	} else {
-	    throw new AlignmentException( "Cannot find URI for "+object2 );
-	}
+
+    public URI getObject2AsURI(Alignment al) throws AlignmentException {
+        if (al instanceof BasicAlignment) {
+            Object ontology = ((BasicAlignment) al).getOntologyObject2();
+            if (ontology instanceof LoadedOntology) {
+                try {
+                    return ((LoadedOntology) ontology).getEntityURI(object2);
+                } catch (OntowrapException owex) {
+                    throw new AlignmentException("Cannot find entity URI(2)", owex);
+                }
+            }
+        }
+        ;
+        if (object2 instanceof URI) {
+            return (URI) object2;
+        } else {
+            throw new AlignmentException("Cannot find URI for " + object2);
+        }
     }
+
     public Cell inverse() throws AlignmentException {
-	Cell result = (Cell)new ObjectCell( (String)null, object2, object1, relation.inverse(), strength );
-	if ( extensions != null ) {
-	    for ( String[] ext : extensions.getValues() ){
-		result.setExtension( ext[0], ext[1], ext[2] );
-	    }
-	    result.setExtension( Namespace.ALIGNMENT.getUriPrefix(), Annotations.ID, (String)null );
-	}
-	// The sae should be done for the measure
-	return result;
+        Cell result = (Cell) new ObjectCell((String) null, object2, object1, relation.inverse(), strength);
+        if (extensions != null) {
+            for (String[] ext : extensions.getValues()) {
+                result.setExtension(ext[0], ext[1], ext[2]);
+            }
+            result.setExtension(Namespace.ALIGNMENT.getUriPrefix(), Annotations.ID, (String) null);
+        }
+        // The sae should be done for the measure
+        return result;
     }
 
     public Cell compose(Cell c) throws AlignmentException {
-    	if (!object2.equals(c.getObject1()) && relation.compose(c.getRelation())==null )
-    		return null;
-    	Cell result = new ObjectCell( (String)null, object1, c.getObject2(), relation.compose(c.getRelation()), strength*c.getStrength() );
-    	// TODO : extension...
-    	return result;
+        if (!object2.equals(c.getObject1()) && relation.compose(c.getRelation()) == null)
+            return null;
+        Cell result = new ObjectCell((String) null, object1, c.getObject2(), relation.compose(c.getRelation()), strength * c.getStrength());
+        // TODO : extension...
+        return result;
     }
 
-    /** Housekeeping **/
-    public void dump( ContentHandler h ){};
+    /**
+     * Housekeeping *
+     */
+    public void dump(ContentHandler h) {
+    }
+
+
 
 }
 
